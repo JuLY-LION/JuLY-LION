fe985ebe2673fb7813f175f71f0b688e0361ab4a
Joshua Nanney <106941080+JuLY-LION@users.noreply.github.com>
2022-06-09 21:40:58 -0400

    Update luanotes.lua
    Got started on relational and logical operators but I can't get a working copy of Lua For Windows installed on my laptop, (so I can't test what I have) and I haven't been able to work on my main PC because either it's occupied or my mother is being insane.

--- src/lua/general/luanotes.lua
+++ src/lua/general/luanotes.lua
@@ -1,4 +1,4 @@
--- JuLY-LION | 6/06/22 --
+-- JuLY-LION | Created 6/06/22 --
 
 print("Variables are dynamic and don't need to be defined.\n")
 name = "Joshua"
@@ -10,12 +10,12 @@ name = 7657
 -- Automatic typecasting! Hooray!
 
 print("type(varName) to get a variable's type. name is currently a ", type(name), "\n")
--- If you variable is a float, it's only precise up to 12 digits.
+-- If your variable is a float, it's only precise up to 13 digits.
 
 print("Concatenation is done with two periods.")
-name = "Joshua"
-fakeName = "Jeremy"
-print(name .. " " .. fakeName .. "\n")
+name = "Jeremy"
+familyName = "Shmitt"
+print(name .. " " .. familyName .. "\n")
 
 print("Lua does not support varName++ or varName += x!\n")
 
@@ -24,8 +24,19 @@ print(math.floor(1.7657))
 print(math.max(6, 9, 4, 2))
 print(math.sqrt(144) .. "\n")
 
-math.randomseed(31)
 print("Want random numbers? math.random is the answer.")
 print(math.random())
 print(math.random(10))
-print(math.random(1,12))
+print(math.random(1,12) .. "\n")
+
+print("Relational: >, <, >=, <=, ==, ~=")
+-- (x != y) won't work!
+print("Logical: and, or, not")
+age = 17
+if age < 15 then
+	print("You may not drive")
+elseif (age == 15 then) --[[ No space on elseif! ]]--
+	print("You may drive with a licensed adult")
+else
+	print("You may drive by yourself")
+end
