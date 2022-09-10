ddd068880d67d18da209b024999d3b4e72ccd412
Joshua Nanney <106941080+JuLY-LION@users.noreply.github.com>
2022-09-09 20:20:38 -0400

    Create inscryption_gm.lua

---
+++ src/lua/games/inscryption_gm.lua
@@ -0,0 +1,13 @@
+
+print("-=[INSCRYPTION DRIVER]=-")
+
+io.write("Would you like to read about this purpose of this program? (y/n)")
+response = io.read()
+if response = "y" or response = "yes" then
+	about()
+end
+local function about()
+	print("So would I!")
+end
+
+io.read()
