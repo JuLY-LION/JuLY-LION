66af10e5d668d71fb4fee92a918e258835469ad5
Joshua Nanney <106941080+JuLY-LION@users.noreply.github.com>
2022-09-10 17:36:28 -0400

    Started setup function

--- src/lua/games/inscryption_gm.lua
+++ src/lua/games/inscryption_gm.lua
@@ -1,6 +1,9 @@
 function about()
 	print("So would I!")
 end
+function setup()
+	print("Enter your starting difficulty: (0-3)")
+end
 print("-=[INSCRYPTION DRIVER]=-")
 
 io.write("Would you like to read about this purpose of this program? (y/n)\n")
@@ -8,6 +11,12 @@ response = io.read()
 if response == "y" or response == "yes" then
 	about()
 end
+
+print("NOTICE: Saving and loading sessions isn't ready yet.")
+print("Starting new session.")
+setup()
+playerCards = 0
+
 
 
 io.read()
