a3381ea4d4060010afa4efbea9f8f53bdcc00e1d
Joshua Nanney <106941080+JuLY-LION@users.noreply.github.com>
2022-09-09 20:28:23 -0400

    Function bug fix
    Turns out functions go on top. Meh.

--- src/lua/games/inscryption_gm.lua
+++ src/lua/games/inscryption_gm.lua
@@ -1,13 +1,13 @@
-
-print("-=[INSCRYPTION DRIVER]=-")
-
-io.write("Would you like to read about this purpose of this program? (y/n)")
-response = io.read()
-if response = "y" or response = "yes" then
-	about()
-end
-local function about()
+function about()
 	print("So would I!")
 end
+print("-=[INSCRYPTION DRIVER]=-")
+
+io.write("Would you like to read about this purpose of this program? (y/n)\n")
+response = io.read()
+if response == "y" or response == "yes" then
+	about()
+end
+
 
 io.read()
